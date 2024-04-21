8709918574b04ee13f24551c69ed37ff72cb01e7
Lucas Pavao <lucaspavao89@gmail.com>
2024-04-21 10:38:20 -0300

    Implementing generate refreshToken

--- APICatalogo/Service/TokenService.cs
+++ APICatalogo/Service/TokenService.cs
@@ -1,7 +1,9 @@
 ﻿using APICatalogo.Interfaces;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace APICatalogo.Service
@@ -32,12 +34,33 @@ namespace APICatalogo.Service
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var secureRadomBytes = new byte[128];
+            using var  randomNumberGenerator = RandomNumberGenerator.Create();
+            randomNumberGenerator.GetBytes(secureRadomBytes);
+            var refreshToken = Convert.ToBase64String(secureRadomBytes);
+            return refreshToken;
         }
 
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config)
         {
-            throw new NotImplementedException();
+            var secretKey = _config["JWT:SecretKey"] ?? throw new InvalidOperationException("Invaid Key");
+            var tokenValidationParameters = new TokenValidationParameters {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                ValidateLifetime = false,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(
+                SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new SecurityTokenException("Invalid token");   
+            }
+            return principal;   
+
         }
     }
 }
--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d1ae94d22434b46d9f8ef8869af878d4e263841")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee2e7695ae8cf088b73ad179e9aaca38a24c1519")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
